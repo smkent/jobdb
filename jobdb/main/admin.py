cb0807d8f8d1fc3d36776f763ff9f231fe6e4968
Stephen Kent <smkent@smkent.net>
2024-06-21 14:19:32 -0700

    Add filters to admin models

--- jobdb/main/admin.py
+++ jobdb/main/admin.py
@@ -1,8 +1,23 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, site
+from django.db.models.query import QuerySet
+from django.http import HttpRequest
 
 from .models import Application, Company, Posting, User
 
-admin.site.register(User)
-admin.site.register(Company)
-admin.site.register(Posting)
-admin.site.register(Application)
+
+class PostingAdmin(ModelAdmin):
+    list_filter = ["company__name"]
+
+
+class ApplicationAdmin(ModelAdmin):
+    list_filter = ["bona_fide"]
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        qs = super().get_queryset(request)
+        return qs.filter(user=request.user)
+
+
+site.register(User)
+site.register(Company)
+site.register(Posting, PostingAdmin)
+site.register(Application, ApplicationAdmin)
