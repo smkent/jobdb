17c1ad0c5c614df7b747e40dd3b2664a537fad0f
Stephen Kent <smkent@smkent.net>
2024-06-21 11:24:48 -0700

    Install django-stubs

--- manage.py
+++ manage.py
@@ -9,9 +9,7 @@ def main() -> None:
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jobdb.settings")
     try:
-        from django.core.management import (  # type: ignore
-            execute_from_command_line,
-        )
+        from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
