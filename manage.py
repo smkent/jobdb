bd33d2f8f06fa03e25b07e49d073c4ec32e2b696
Stephen Kent <smkent@smkent.net>
2024-06-21 11:11:13 -0700

    Automatically invoke manage.py through Poetry

--- manage.py
+++ manage.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
+import subprocess
 import sys
 
 
-def main():
+def main() -> None:
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jobdb.settings")
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.management import (  # type: ignore
+            execute_from_command_line,
+        )
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -19,4 +22,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    if os.environ.get("VIRTUAL_ENV"):
+        main()
+    else:
+        subprocess.run(["poetry", "run", "python"] + sys.argv)
