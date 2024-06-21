1133502e32ddf4f6e7d40b32669492a90d03099b
Stephen Kent <smkent@smkent.net>
2024-06-21 14:22:38 -0700

    Remove job applications view per-user filter

--- jobdb/main/admin.py
+++ jobdb/main/admin.py
@@ -1,6 +1,4 @@
 from django.contrib.admin import ModelAdmin, site
-from django.db.models.query import QuerySet
-from django.http import HttpRequest
 
 from .models import Application, Company, Posting, User
 
@@ -11,10 +9,6 @@ class PostingAdmin(ModelAdmin):
 
 class ApplicationAdmin(ModelAdmin):
     list_filter = ["bona_fide"]
-
-    def get_queryset(self, request: HttpRequest) -> QuerySet:
-        qs = super().get_queryset(request)
-        return qs.filter(user=request.user)
 
 
 site.register(User)
