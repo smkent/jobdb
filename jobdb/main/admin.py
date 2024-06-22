0b35fe497a74a8cfdb02621f603c84ab1ee404a1
Stephen Kent <smkent@smkent.net>
2024-06-21 20:48:51 -0700

    Customize admin models

--- jobdb/main/admin.py
+++ jobdb/main/admin.py
@@ -1,19 +1,101 @@
-from django.contrib.admin import ModelAdmin, site
+from functools import partial
+
+from django.contrib.admin import ModelAdmin, display, register
+from django.utils.html import format_html
 
 from ..admin import personal_admin_site
 from .models import Application, Company, Posting, User
 
+register_portal = partial(register, site=personal_admin_site)
 
+
+def clickable_url_html(
+    url: str, display: str = "", target: str = "_blank"
+) -> str:
+    return format_html(
+        '<a href="{0}" target="{2}">{1}</a>', url, display or url, target
+    )
+
+
+class UserProxy(User):
+    class Meta:
+        proxy = True
+        app_label = "auth"
+        verbose_name = User._meta.verbose_name
+
+
+@register(UserProxy)
+class UserAdmin(ModelAdmin):
+    ordering = ["username"]
+
+
+@register(Company)
+@register_portal(Company)
+class CompanyAdmin(ModelAdmin):
+    list_display = ["name", "url_clickable", "careers_url_clickable", "hq"]
+    ordering = ["name"]
+
+    @display(description=Company._meta.get_field("url").verbose_name)
+    def url_clickable(self, obj: Company) -> str:
+        return clickable_url_html(obj.url)
+
+    @display(description=Company._meta.get_field("careers_url").verbose_name)
+    def careers_url_clickable(self, obj: Company) -> str:
+        return clickable_url_html(obj.careers_url)
+
+
+@register(Posting)
+@register_portal(Posting)
 class PostingAdmin(ModelAdmin):
+    list_display = [
+        "company_name",
+        "title",
+        "url_clickable",
+        "is_closed",
+        "location",
+    ]
+    list_display_links = ["title"]
     list_filter = ["company__name"]
+    ordering = ["company__name", "title", "url"]
+
+    @display(description=Company._meta.get_field("name").verbose_name)
+    def company_name(self, obj: Company) -> str:
+        assert isinstance(obj.company.name, str)
+        return clickable_url_html(
+            f"../company/{obj.company.pk}/change/",
+            obj.company.name,
+            target="_self",
+        )
+
+    @display(description=Posting._meta.get_field("url").verbose_name)
+    def url_clickable(self, obj: Posting) -> str:
+        return clickable_url_html(obj.url)
+
+    @display(description="Closed")
+    def is_closed(self, obj: Posting) -> str:
+        return "Yes" if bool(obj.closed) else "No"
 
 
-class ApplicationAdmin(ModelAdmin):
-    list_filter = ["bona_fide"]
+@register_portal(Application)
+class ApplicationPortalAdmin(ModelAdmin):
+    list_display = ["summary", "applied", "reported", "bona_fide"]
+    list_display_links = ["summary"]
+    list_filter = ["bona_fide", "posting__company__name"]
+    ordering = [
+        "-applied",
+        "user",
+        "posting__company__name",
+        "posting__title",
+        "posting__url",
+    ]
+
+    @display(description="Application")
+    def summary(self, obj: Application) -> str:
+        return f"{obj.posting.company.name} • {obj.posting.title}"
 
 
-site.register(Posting, PostingAdmin)
-site.register(Application, ApplicationAdmin)
-site.register(Company)
-site.register(User)
-personal_admin_site.register(Company)
+@register(Application)
+class ApplicationAdmin(ApplicationPortalAdmin):
+    list_display = [
+        "user"
+    ] + ApplicationPortalAdmin.list_display  # type: ignore
