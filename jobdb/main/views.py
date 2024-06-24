55145cdd77e0ff3a5de5b0686fa6347f517adcdc
Stephen Kent <smkent@smkent.net>
2024-06-23 23:51:55 -0700

    Add custom login form

--- jobdb/main/views.py
+++ jobdb/main/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 
 
+@login_required
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Greetings human")
+    return render(request, "index.html")
--- jobdb/urls.py
+++ jobdb/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 from .admin import personal_admin_site
 
 urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
     path("admin/", admin_site.urls),
     path("portal/", personal_admin_site.urls),
     path("api/", include("jobdb.api.urls")),
