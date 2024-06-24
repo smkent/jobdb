fd52a5f2edfe47b3233a7dc703a273c7fc956f9a
Stephen Kent <smkent@smkent.net>
2024-06-24 00:24:30 -0700

    Add API endpoints for object retrieval by company name, posting URL

--- jobdb/api/urls.py
+++ jobdb/api/urls.py
@@ -10,8 +10,17 @@ from . import views
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r"companies", views.CompanyViewSet)
+router.register(
+    r"companies/by_name", views.CompanyByNameViewSet, "company-by-name"
+)
 router.register(r"postings", views.PostingViewSet)
+router.register(
+    r"postings/by_url", views.PostingByURLViewSet, "posting-by-url"
+)
 router.register(r"applications", views.ApplicationViewSet)
+router.register(
+    r"applications/by_url", views.ApplicationByURLViewSet, "application-by-url"
+)
 
 urlpatterns = [
     path("", include(router.urls)),
