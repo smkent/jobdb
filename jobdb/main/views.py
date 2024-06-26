1a8d62bab2bf7871db1493baaed7df7856ff9516
Stephen Kent <smkent@smkent.net>
2024-06-25 21:44:09 -0700

    Add some landing page statistics

--- jobdb/main/views.py
+++ jobdb/main/views.py
@@ -2,7 +2,19 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from .models import Application, Company, Posting
+
 
 @login_required
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "index.html")
+    your_apps = Application.objects.filter(user=request.user)
+    return render(
+        request,
+        "index.html",
+        {
+            "company": Company.objects.all(),
+            "posting": Posting.objects.all(),
+            "application": Application.objects.all(),
+            "your_apps": your_apps,
+        },
+    )
