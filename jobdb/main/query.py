35b0b945c46a4aeb69c97c81e133c5a082f4d30c
Stephen Kent <smkent@smkent.net>
2024-06-27 23:33:54 -0700

    Implement bulk admin action for creating applications for postings

--- jobdb/main/query.py
+++ jobdb/main/query.py
@@ -1,19 +1,23 @@
-from django.db.models import Case, Count, Max, Q, QuerySet, When
-from django.db.models.expressions import Value
+from django.db.models import Case, Count, F, Max, Q, QuerySet, When
 
 from .models import Company, Posting, User
 
 
-def posting_queue_set(user: User, ordered: bool = True) -> QuerySet:
-    qs = (
-        Posting.objects.filter(closed__isnull=True)
-        .annotate(
-            has_application=Max(
-                Case(When(application__user=user, then=Value(1)))
-            )
+def posting_with_applications(
+    user: User, queryset: QuerySet | None = None
+) -> QuerySet:
+    queryset = queryset or Posting.objects.all()
+    return queryset.annotate(
+        has_application=Max(
+            Case(When(application__user=user, then=F("application__pk")))
         )
-        .filter(Q(has_application=False) | Q(has_application__isnull=True))
     )
+
+
+def posting_queue_set(user: User, ordered: bool = True) -> QuerySet:
+    qs = posting_with_applications(
+        user=user, queryset=Posting.objects.filter(closed__isnull=True)
+    ).filter(Q(has_application__isnull=True))
     if ordered:
         qs = qs.order_by("company__name", "title", "url")
     return qs
