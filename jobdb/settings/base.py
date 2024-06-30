78e623e93b2ad3c594a8f8192ba5dbffd80cd07a
Stephen Kent <smkent@smkent.net>
2024-06-30 09:35:57 -0700

    Add "(Dev)" to base template header when settings.DEBUG is True

--- jobdb/settings/base.py
+++ jobdb/settings/base.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "jobdb.utils.context_processors.common_context",
             ],
         },
     },
