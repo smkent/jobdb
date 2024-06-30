9fd560c47d3c1464ff5dc950a265b778bcebb4b3
Stephen Kent <smkent@smkent.net>
2024-06-30 09:38:50 -0700

    Move url_to_text to main/utils.py

--- jobdb/main/models.py
+++ jobdb/main/models.py
@@ -26,26 +26,7 @@ from django.db.models import (
 from django_extensions.db.models import TimeStampedModel  # type: ignore
 
 from .fields import AppliedDateField, URLArray
-
-
-def url_to_text(url: str) -> str:
-    netloc_max = 25
-    path_max = 27
-    url_parts = urlparse(url)
-    split_data = url_parts.path + (
-        "?" + url_parts.query if url_parts.query else ""
-    )
-    bits = split_data.rsplit("/", maxsplit=2)[-2:]
-    path_bits = ("/" + "/".join([bit for bit in bits if bit]))[-path_max:]
-    netloc_bits = (
-        url_parts.netloc[: (netloc_max - 3)] + "[...]"
-        if len(url_parts.netloc) > netloc_max
-        else url_parts.netloc
-    )
-    if path_bits == split_data:
-        return f"{netloc_bits}{path_bits}"
-    else:
-        return f"{netloc_bits}/[...]{path_bits}"
+from .utils import url_to_text
 
 
 class User(AbstractUser):
